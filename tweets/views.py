e8bdfd48cdb50f18c91b27c209342c47fc179cab
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2021-06-05 19:41:45 +0530

    converting django view to rest api view

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
+from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
@@ -8,5 +10,20 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIWE
+    Conume by JavaScript/Swift/Java/iOS/Android
+    return json data
+    """
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
 
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    data = {
+        "id": tweet_id,
+        "content": obj.content,
+        # "image_path": obj.image.url
+    }
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+    return JsonResponse(data)
